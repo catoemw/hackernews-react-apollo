b11f038fdc60d000db7f0116a398384bfc6e587b
Matt Catoe <catoemw@gmail.com>
2021-03-14 19:55:09 -0400

    Add count to Feed schema

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -12,9 +12,12 @@ async function feed(parent, args, context) {
     where,
     skip: args.skip,
     take: args.take,
+    orderBy: args.orderBy,
   });
 
-  return links;
+  const count = await context.prisma.link.count({ where });
+
+  return { links, count };
 }
 
 module.exports = {
